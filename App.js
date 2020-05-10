4f3e5a1a7c394978aac2b09ba3349fc94bfa2431
Ashkan <ash.larni@gmail.com>
2020-05-10 18:20:03 +0430

    Front-end pushed

--- App.js
+++ App.js
@@ -1,19 +1,115 @@
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Asset } from 'expo-asset';
+import { AppLoading } from 'expo';
+import { Container } from 'native-base';
+import * as Font from 'expo-font';
+import { FontAwesome } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import Frate from './app/index';
+import SignUp from './app/signup'
+
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 }
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
+// export default class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       isReady: false
+//     };
+//   }
+
+//   async _loadAssetsAsync() {
+//     const imageAssets = cacheImages([require('./assets/images/bg.jpg')]);
+
+//     await Promise.all([...imageAssets]);
+//   }
+
+//   render() {
+//     if (!this.state.isReady) {
+//       return (
+//         <AppLoading
+//           startAsync={this._loadAssetsAsync}
+//           onFinish={() => this.setState({ isReady: true })}
+//           onError={console.warn}
+//         />
+//       );
+//     }
+//     return <Frate />;
+//   }
+// }
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false
+    };
+  }
+
+  async componentDidMount() {
+    const imageAssets = cacheImages([
+      require('./assets/images/bg.jpg'),
+      require('./assets/images/bg2.png'),
+    ]);
+
+    const fontAssets = cacheFonts([FontAwesome.font]);
+
+    await Font.loadAsync({
+      OpenSans_Bold: require("./assets/fonts/Open_Sans/OpenSans-Bold.ttf"),
+      OpenSans_SemiBoldItalic: require("./assets/fonts/Open_Sans/OpenSans-SemiBoldItalic.ttf"),
+      OpenSans_SemiBold: require("./assets/fonts/Open_Sans/OpenSans-SemiBold.ttf"),
+      ...Ionicons.font,
+    });
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+    // await Font.loadAsync({
+    //   IRANSans: require('./assets/fonts/IRANSansMobile.ttf'),
+    //   IRANSans_bold: require('./assets/fonts/IRANSansMobile_Bold.ttf'),
+    //   IRANSans_medium: require('./assets/fonts/IRANSansMobile_Medium.ttf'),
+    // })
+    this.setState({ isReady: true });
+  }
+
+
+  render() {
+    if (!this.state.isReady) {
+      return <Frate />;
+    }
+
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Login'>
+          <Stack.Screen name='Sign Up' component={SignUp} />
+          <Stack.Screen name='Login' component={Frate} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
---
+++ app/signup.js
@@ -0,0 +1,74 @@
+//This is an example code for Navigator// 
+import * as React from 'react';
+import { View, Text, StyleSheet, Image, Dimensions, TextInput } from 'react-native';
+import { Button } from 'native-base';
+
+const { width, height } = Dimensions.get('window');
+
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+    render() {
+        // const { navigate } = this.props.navigation;
+        return (
+            <View style={{ flex: 1, backgroundColor: '#f2f2f2', justifyContent: 'flex-end' }}>
+                <View style={StyleSheet.absoluteFill}>
+                    <Image
+                        source={require('../assets/images/bg2.png')}
+                        style={{ flex: 1, height: null, width: null }}
+                    />
+                </View>
+                <View style={{ height: height, justifyContent: 'center' }}>
+                    <TextInput placeholder='USERNAME' style={styles.textInput} placeholderTextColor='rgba(0, 0, 0, 0.6)' />
+                    <TextInput placeholder='EMAIL' style={styles.textInput} placeholderTextColor='rgba(0, 0, 0, 0.6)' />
+                    <TextInput placeholder='PASSWORD' style={styles.textInput} placeholderTextColor='rgba(0, 0, 0, 0.6)' secureTextEntry={true} />
+                    <TextInput placeholder='RE-ENTER PASSWORD' style={styles.textInput} placeholderTextColor='rgba(0, 0, 0, 0.6)' secureTextEntry={true} />
+                    <Button rounded style={{ ...styles.button, marginVertical: 50, backgroundColor: 'rgb(85, 205, 95)' }}>
+                        <Text style={{ fontSize: 20, fontFamily: 'OpenSans_Bold' }}>
+                            SIGN UP
+                        </Text>
+                    </Button>
+                </View>
+            </View>
+        );
+    }
+}
+export default SignUp;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        margin: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        backgroundColor: 'white',
+        height: 65,
+        marginHorizontal: 50,
+        marginVertical: 7.5,
+        borderRadius: 32.5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowOffset: { height: 2 },
+        shadowColor: 'black',
+        shadowOpacity: 0.4,
+        elevation: 5
+    },
+    textInput: {
+        height: 50,
+        borderRadius: 25,
+        // borderWidth: 0.5,
+        backgroundColor: 'white',
+        // borderColor: 'rgba(0, 0, 0, 0.5)',
+        marginHorizontal: 50,
+        marginVertical: 15,
+        paddingLeft: 20,
+        fontWeight: 'bold',
+        shadowColor: 'black',
+        shadowOpacity: 0.2
+    }
+});
