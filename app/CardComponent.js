b8fae12c54a1d97562ece4575fab562c8e904ad5
Ashkan <ash.larni@gmail.com>
2020-06-12 18:07:03 +0430

    Ratings added, props added, timeline in a good state :)

--- app/CardComponent.js
+++ app/CardComponent.js
@@ -10,55 +10,48 @@ const { width, height } = Dimensions.get('window');
 // create a component
 class CardComponent extends Component {
     render() {
-
-        const images = {
-            "1": require('../assets/images/feed/1.jpg'),
-            "2": require('../assets/images/feed/2.jpg'),
-            "3": require('../assets/images/feed/3.jpg')
-        }
-
         return (
-                    <Card>
-                        <CardItem>
-                            <Left>
-                                <Thumbnail
-                                    source={require('../assets/images/me.png')}
-                                />
-                                <Body>
-                                    <Text style={{ fontFamily: 'Roboto_Regular' }}>Ashkan</Text>
-                                    <Text note style={{ fontFamily: 'Roboto_Light' }}>Jun 9, 2020</Text>
-                                </Body>
-                            </Left>
-                        </CardItem>
-                        <CardItem cardBody>
-                            <Image
-                                source={images[this.props.imageSource]}
-                                style={{ height: width, width: null, flex: 1 }}
-                            />
-                        </CardItem>
-                        <CardItem style={{ height: 50 }}>
-                            <Right>
-                                <ProgressCircle
-                                    percent={30}
-                                    radius={18}
-                                    borderWidth={4}
-                                    color="#3399FF"
-                                    shadowColor="#d3d3d3"
-                                    bgColor="white"
-                                >
-                                    <Text style={{ fontSize: 12, fontFamily: 'Roboto_Light' }} >{[this.props.rate]}</Text>
-                                </ProgressCircle>
-                            </Right>
-                        </CardItem>
-                        <CardItem>
-                            <Body>
-                                <Text>
-                                    <Text style={{ fontWeight: "900" }}>varun</Text>
-                                    {' '}{'Ea do Lorem occaecat laborum do. Minim ullamco ipsum minim eiusmod dolore cupidatat magna exercitation amet proident qui. Est do irure magna dolor adipisicing do quis labore excepteur. Commodo veniam dolore cupidatat nulla consectetur do nostrud ea cupidatat ullamco labore. Consequat ullamco nulla ullamco minim.'}
-                                </Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
+            <Card>
+                <CardItem>
+                    <Left>
+                        <Thumbnail
+                            source={this.props.profilePicSource}
+                        />
+                        <Body>
+                            <Text style={{ fontFamily: 'Roboto_Regular' }}>{this.props.name}</Text>
+                            <Text note style={{ fontFamily: 'Roboto_Light' }}>{this.props.date}</Text>
+                        </Body>
+                    </Left>
+                </CardItem>
+                <CardItem cardBody>
+                    <Image
+                        source={this.props.imageSource}
+                        style={{ height: width, width: null, flex: 1 }}
+                    />
+                </CardItem>
+                <CardItem style={{ height: 50 }}>
+                    <Right>
+                        <ProgressCircle
+                            percent={30}
+                            radius={18}
+                            borderWidth={4}
+                            color="#3399FF"
+                            shadowColor="#d3d3d3"
+                            bgColor="white"
+                        >
+                            <Text style={{ fontSize: 12, fontFamily: 'Roboto_Light' }} >{[this.props.rate[1]]}</Text>
+                        </ProgressCircle>
+                    </Right>
+                </CardItem>
+                <CardItem>
+                    <Body>
+                        <Text>
+                            <Text style={{ fontWeight: "900" }}>{this.props.name}</Text>
+                            {this.props.caption}
+                        </Text>
+                    </Body>
+                </CardItem>
+            </Card>
         );
     }
 }
--- app/Home.js
+++ app/Home.js
@@ -5,6 +5,24 @@ import { Container, Content } from 'native-base';
 
 import CardComponent from './CardComponent';
 
+const images = {
+    "1": require('../assets/images/feed/1.jpg'),
+    "2": require('../assets/images/feed/2.jpg'),
+    "3": require('../assets/images/feed/3.jpg')
+}
+
+const profilePic = {
+    "1": require('../assets/images/profile/Ashkan.png'),
+    "2": require('../assets/images/profile/Ali.jpg'),
+    "3": require('../assets/images/profile/Shayesteh.jpg'),
+}
+
+const caption = {
+    "1": ' Ea do Lorem occaecat laborum do. Minim ullamco ipsum minim eiusmod dolore cupidatat magna exercitation amet proident qui. Est do irure magna dolor adipisicing do quis labore excepteur.',
+    "2": ' Minim ullamco ipsum minim eiusmod dolore cupidatat magna exercitation amet proident qui. Est do irure magna dolor adipisicing do quis labore excepteur.',
+    "3": ' Est do irure magna dolor adipisicing do quis labore excepteur.'
+}
+
 // create a component
 class Home extends Component {
 
@@ -12,9 +30,33 @@ class Home extends Component {
         return (
             <Container style={styles.container}>
                 <Content>
-                    <CardComponent imageSource="1" rate="1.5" />
-                    <CardComponent imageSource="2" rate="2.6" />
-                    <CardComponent imageSource="3" rate="3.7" />
+                    <CardComponent
+                        name="Ashkan"
+                        date="Jun 9, 2020"
+                        profilePicSource={profilePic[1]}
+                        imageSource={images[1]}
+                        category="lifestyle"
+                        rate={[1.5, 2.5, 3.5, 4.5]}
+                        caption={caption[1]}
+                    />
+                    <CardComponent
+                        name="Ali"
+                        date="Feb 14, 2020"
+                        profilePicSource={profilePic[2]}
+                        imageSource={images[2]}
+                        category="photography"
+                        rate={[1.7, 2.7, 3.7, 4.7]}
+                        caption={caption[2]}
+                    />
+                    <CardComponent
+                        name="Shayesteh"
+                        date="Aug 2, 2019"
+                        profilePicSource={profilePic[3]}
+                        imageSource={images[3]}
+                        category="art"
+                        rate={[1.9, 2.9, 3.9, 4.9]}
+                        caption={caption[3]}
+                    />
                 </Content>
             </Container>
         );
