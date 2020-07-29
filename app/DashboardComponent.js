532b1d92739be7f2f88f43282a4b9290d9985848
Ashkan <ash.larni@gmail.com>
2020-07-29 20:49:18 +0430

    some minor changes.

--- app/DashboardComponent.js
+++ app/DashboardComponent.js
@@ -54,53 +54,52 @@ class DashboardComponent extends Component {
                                     </View>
                                 </CardItem>
                                 <CardItem style={styles.cardItem}>
-                                    <View style={{ width: width / 2, alignItems: 'center' }}>
-                                        <Button transparent style={{ width: width / 2, justifyContent: 'center' }}>
+                                    <View style={{ flexDirection: 'row', width: width, alignItems: 'center', justifyContent: 'center' }}>
+                                        <Button transparent style={{ width: width / 2, justifyContent: 'center', flexDirection: 'column' }}>
                                             <Text style={styles.samsungSans}> {'Followers'}</Text>
+                                            <Text style={styles.samsungSans}>{'290'}</Text>
+                                        </Button>
+                                        <Button transparent style={{ width: width / 2, justifyContent: 'center', flexDirection: 'column' }}>
+                                            <Text style={styles.samsungSans}>{'Followings'}</Text>
+                                            <Text style={styles.samsungSans}>{'123'}</Text>
                                         </Button>
                                     </View>
-                                    {/* <View style={{ width: width / 3, alignItems: 'center' }}>
-                                        <Text style={styles.samsungSans}>{'|'}</Text>
-                                    </View> */}
-                                    <Button transparent style={{ width: width / 2, justifyContent: 'center' }}>
-                                        <Text style={styles.samsungSans}>{'Followings'}</Text>
-                                    </Button>
                                 </CardItem>
                                 <CardItem style={styles.cardItem}>
-                                    <View style={{ flexDirection: 'row', flex: 1, width: width, justifyContent: 'space-around', backgroundColor: 'red', borderRadius: 5 }}>
-                                        <View style={{ width: width / 3, height: 200, alignItems: 'center' }}>
+                                    <View style={{ flexDirection: 'row', flex: 1, width: width, justifyContent: 'space-around', backgroundColor: '#fbfbfb', borderRadius: 5 }}>
+                                        <View style={styles.progressCircleView}>
                                             <Text style={{ ...styles.samsungSans, paddingBottom: 10 }}>{'Art'}</Text>
                                             <ProgressCircle
                                                 percent={2.9 / 5 * 100}
                                                 radius={18}
                                                 borderWidth={3}
-                                                color={'blue'}
+                                                color={'#0080ff'}
                                                 shadowColor="#d3d3d3"
                                                 bgColor="white"
                                             >
                                                 <Text style={styles.progressCircleText} >{'2.9'}</Text>
                                             </ProgressCircle>
                                         </View>
-                                        <View style={{ width: width / 3, height: 200, flex: 1, alignItems: 'center' }}>
+                                        <View style={styles.progressCircleView}>
                                             <Text style={{ ...styles.samsungSans, paddingBottom: 10 }}>{'Photography'}</Text>
                                             <ProgressCircle
                                                 percent={2.9 / 5 * 100}
                                                 radius={18}
                                                 borderWidth={3}
-                                                color={'red'}
+                                                color={'#e0115f'}
                                                 shadowColor="#d3d3d3"
                                                 bgColor="white"
                                             >
                                                 <Text style={styles.progressCircleText} >{'2.9'}</Text>
                                             </ProgressCircle>
                                         </View>
-                                        <View style={{ width: width / 3, height: 200, flex: 1, alignItems: 'center' }}>
+                                        <View style={styles.progressCircleView}>
                                             <Text style={{ ...styles.samsungSans, paddingBottom: 10 }}>{'Lifestyle'}</Text>
                                             <ProgressCircle
                                                 percent={2.9 / 5 * 100}
                                                 radius={18}
                                                 borderWidth={3}
-                                                color={'green'}
+                                                color={'#50c878'}
                                                 shadowColor="#d3d3d3"
                                                 bgColor="white"
                                             >
@@ -124,7 +123,7 @@ class DashboardComponent extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fdfdfd',
+        backgroundColor: '#ffffff',
     },
     title: {
         fontFamily: 'Vision_Black',
@@ -144,7 +143,12 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         width: width,
-        backgroundColor: '#fdfdfd'
+        backgroundColor: '#ffffff'
+    },
+    progressCircleView: {
+        width: width / 3,
+        alignItems: 'center',
+        marginVertical: 20
     }
 });
 
